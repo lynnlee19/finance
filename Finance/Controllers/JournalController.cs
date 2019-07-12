9d7f8959c7d29bfdec772aa0255c6536f42ab34a
unknown <jameilee@yfp.local>
2019-07-12 13:43:42 +0800

    加入專案檔案。

---
+++ Finance/Controllers/JournalController.cs
@@ -0,0 +1,40 @@
+﻿using Bogus;
+using Finance.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Finance.Controllers
+{
+    public class JournalController : Controller
+    {
+        // GET: Journal
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [ChildActionOnly]
+        public ActionResult SubAction()
+        {
+
+            var faker = new Faker("zh_TW");
+            var journals = new List<JournalViewModel>();
+            for (int i = 0; i < 20; i++)
+            {
+                journals.Add(new JournalViewModel
+                {
+                    TxnDate = faker.Date.Past(),
+                    TxnType = (EnumTxnType) faker.Random.Number(min: 1, max: 2),
+                    TxnAmt = faker.Finance.Amount(min: 1, max: 5000)
+                });
+            }
+
+            journals.Sort();
+
+            return View(journals);
+        }
+    }
+}
---
+++ Finance/Models/ViewModels/JournalViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Finance.Models.ViewModels
+{
+    public enum EnumTxnType { 收入 = 1, 支出 = 2 };
+
+    public class JournalViewModel: IComparable<JournalViewModel>
+    {
+        [Display(Name = "日期", Description = "交易日期")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
+        public System.DateTime TxnDate { get; set; }
+
+        [Display(Name = "類別", Description = "類別")]
+        public EnumTxnType TxnType { get; set; }
+
+        [Display(Name = "備註", Description = "交易備註")]
+        public string Description { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:N0}", ApplyFormatInEditMode = true)]
+        [Display(Name = "金額", Description = "交易金額")]
+        public decimal TxnAmt { get; set; }
+
+        public int CompareTo(JournalViewModel other)
+        {
+            if (other == null)
+                return 1;
+
+            else
+                return this.TxnDate.CompareTo(other.TxnDate);
+        }
+    }
+}
